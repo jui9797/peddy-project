906fe388c5d94ef8a9cbd1e72805a77d986f5f5a
jui9797 <jannatjui@414gmail.com>
2024-10-06 21:13:27 +0600

    added some function

--- js/pet.js
+++ js/pet.js
@@ -17,12 +17,36 @@ const displayCategoryBtn =(categories)=>{
     const div =document.createElement('div')
    
     div.innerHTML = `
-    <button class="btn w-full lg:w-72 lg:h-20 font-inter  lg:text-2xl lg:px-4 lg:py-2 border rounded-xl"><img class="w-9 h-9 lg:w-14 lg:h-14" src=${category.category_icon}>
+    <button  id="${category.category}"  onclick="loadCategoryPets('${category.category}')" class=" remove-active  btn w-full lg:w-72 lg:h-20 font-inter  lg:text-2xl lg:px-4 lg:py-2 border rounded-xl"><img class="w-9 h-9 lg:w-14 lg:h-14" src=${category.category_icon}>
     ${category.category}</button>
     `;
     buttonContainer.append(div);
     });
 
+}
+
+// load category videos
+const loadCategoryPets =async(category) =>{
+    // alert(category)
+const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+const data = await res.json()
+
+// remove active class
+removeActive();
+
+// add active class
+const activeBtn =document.getElementById(`${category}`)
+activeBtn.classList.add('btn-style');
+// console.log(activeBtn);
+displayAllPets(data.data);
+}
+
+const removeActive =()=>{
+  const buttons =document.getElementsByClassName('remove-active') 
+//   console.log(buttons);
+  for(let button of buttons) {
+    button.classList.remove("btn-style");
+  }
 }
 
 // loading spinner
@@ -48,6 +72,25 @@ const loadAllPets =async() =>{
 const displayAllPets =(pets) =>{
     // console.log(pets);
     const cardContainer =document.getElementById('card-container')
+    cardContainer.innerHTML ="";
+
+   if(pets.length == 0){
+    cardContainer.classList.remove('grid')
+    cardContainer.classList.add('border','p-4', 'bg-base-200', 'rounded-lg')
+    cardContainer.innerHTML =`
+    <div class="flex flex-col gap-5 justify-center items-center">
+    <img src="images/error.webp">
+    <h2 class="font-inter font-extrabold text-3xl text-title">No Information Available</h2>
+    <p class="font-lato text-para text-center">It is a long established fact that a reader will be distracted by the readable content of a page when looking at 
+    its layout. The point of using Lorem Ipsum is that it has a.</p>
+    </div>
+    `;
+    return;
+   }
+   else{
+    cardContainer.classList.add('grid')
+   }
+
   pets.forEach((pet) =>{
     const {image, breed, date_of_birth, gender, pet_name, price, petId} =pet;
     // console.log(pet)
@@ -68,7 +111,7 @@ const displayAllPets =(pets) =>{
                   <p class="text-para"><i class="fa-solid fa-dollar-sign mr-2"></i> Price: <span>${price? `${price} $` : 'Not available'}</span></p>
                   <hr>
                   <div class="flex gap-2 ">
-                    <button class="btn "><i class="fa-regular fa-thumbs-up"></i></button>
+                    <button onclick="showImage('${petId}')" class="btn "><i class="fa-regular fa-thumbs-up"></i></button>
                     <button class="btn text-btn font-bold">Adopt</button>
                     <button onclick=showDetails('${petId}') class="btn text-btn font-bold">Details</button>
                   </div>
@@ -122,6 +165,23 @@ modal.innerHTML =`
 
 
 modal.showModal()
+}
+
+// show pet image by pressing like btn
+const showImage =async (id)=>{
+// console.log(id)
+const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${id}`)
+const data = await res.json()
+console.log(data.petData);
+const imageContainer =document.getElementById('image')
+console.log(imageContainer);
+const div =document.createElement('div')
+div.innerHTML =`
+
+<img src=${data.petData.image}>
+
+`;
+imageContainer.append(div);
 }
 
 
